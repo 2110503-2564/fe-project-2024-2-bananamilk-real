3aa63fedff2f007617fa7002e1ccccbe4df41388
Patsakorn Petcharak <patsakorn.thumb@gmail.com>
2025-03-25 18:35:13 +0700

    delete unnecessary files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ export default function HomePage() {
   }, []);
 
   return (
-    <main className="p-6 relative h-screen w-screen">
+    <main className="p-6 relative">
       <Banner />
-      <div className="flex flex-col items-center !my-5">
-        <h1 className="text-3xl font-bold text-gray-400 font-serif !mt-1">
+      <div className="flex flex-col items-center mt-8">
+        <h1 className="text-3xl font-bold text-gray-700 mb-6">
           Start Skipping Lines Now
         </h1>
         <Image 
@@ -25,19 +25,18 @@ export default function HomePage() {
           alt="Logo"
           width={180}
           height={180}
-          className="!my-10 rounded-lg w-[20%] h-auto shadow-2xl hover:shadow-lg transform transition-all duration-600 ease-in-out hover:scale-105"
+          className="mb-10"
         />
         <button
-          className="bg-white text-cyan-600 border border-cyan-600 font-semibold !p-3 rounded transform transition-all duration-300 ease-in-out hover:scale-105 hover:bg-cyan-600 hover:text-white hover:border-transparent cursor-pointer"
+          className="bg-white text-cyan-600 border border-cyan-600 font-semibold py-2 px-4 rounded transform transition-all duration-300 ease-in-out hover:scale-105 hover:bg-cyan-600 hover:text-white hover:border-transparent"
           onClick={(e) => {
             e.stopPropagation();
             router.push("/restaurants");
           }}
         >
-          View Restaurant
+          Select Restaurant
         </button>
       </div>
-      <div className="absolute bottom-0 right-5 font-small text-gray-400">by Bananamilk</div>
     </main>
   );
 }
